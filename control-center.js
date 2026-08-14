4f5bded2cd19e65a70dcc7b9b8f5638bfd9eda5a
Samster68OW <samster6868@gmail.com>
2026-08-14 17:55:53 -0400

    Aug 14th Push 3

--- control-center.js
+++ control-center.js
@@ -19,7 +19,7 @@ const huntList = [
 // Set this to the index of the hunt above.
 const activeHunt = 4;
 // Set the status of the active hunt. (false = coming soon, true = available)
-const activeHuntStatus = false;
+const activeHuntStatus = true;
 
 
 
